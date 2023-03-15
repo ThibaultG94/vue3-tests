33e812bd6266bad4829d35f3fd05f67f0e16d204
Thibault Guilhem <thibault.guilhem@gmail.com>
2023-03-15 14:24:03 +0100

    emit

---
+++ emit/src/components/HelloWorld.vue
@@ -0,0 +1,41 @@
+<template>
+	<div>
+		<!-- <input
+			type="text"
+			placeholder="Ecris du texte pertinent"
+			@change="$emit('listenChange', $event.target.value)" /> -->
+		<input
+			type="text"
+			placeholder="Sois pertinent"
+			@change="listenChange" />
+	</div>
+</template>
+
+<script>
+export default {
+	name: 'HelloWorld',
+	methods: {
+		listenChange(event) {
+			this.$emit('listenChange', event.target.value);
+		},
+	},
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+	margin: 40px 0 0;
+}
+ul {
+	list-style-type: none;
+	padding: 0;
+}
+li {
+	display: inline-block;
+	margin: 0 10px;
+}
+a {
+	color: #42b983;
+}
+</style>
